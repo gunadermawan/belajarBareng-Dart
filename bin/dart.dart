24ec5b37bbf2f4178d09b4d947650db17fb1eb4a
itselaras <gunadermawan62@gmail.com>
2022-08-10 11:27:51 +0700

    course variable

--- bin/dart.dart
+++ bin/dart.dart
@@ -2,4 +2,9 @@ import 'package:dart/dart.dart' as dart;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart.calculate()}!');
+  // variable
+  var name = "gunadermawan";
+  print(name);
+  var age = 12;
+  print(age + 2);
 }
