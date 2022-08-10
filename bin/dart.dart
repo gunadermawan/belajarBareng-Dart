2a4e10a9e8a79edadabed14a033fdab595dc1b88
itselaras <gunadermawan62@gmail.com>
2022-08-10 13:15:48 +0700

    update course dart i/o

--- bin/dart.dart
+++ bin/dart.dart
@@ -1,7 +1,13 @@
 import 'package:dart/dart.dart' as dart;
+import 'dart:io';
 
 void main(List<String> arguments) {
+  // hello world
   print('Hello world: ${dart.calculate()}!');
+  // dart I/O
+  print("input password");
+  String? inputText = stdin.readLineSync()!;
+  print("password: $inputText");
   // variable
   var name = "gunadermawan";
   print(name);
