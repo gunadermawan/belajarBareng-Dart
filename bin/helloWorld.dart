d816709887a66967fdb91d9b56d2f645c0341bd7
itselaras <gunadermawan62@gmail.com>
2022-08-11 09:37:13 +0700

    move course to make file

---
+++ bin/helloWorld.dart
@@ -0,0 +1,6 @@
+import 'package:dart/dart.dart' as dart;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart.calculate()}!');
+}
---
+++ bin/variable.dart
@@ -0,0 +1,12 @@
+void main(List<String> arguments) {
+  // variable
+  var name = "gunadermawan";
+  print(name);
+  var age = 12;
+  print(age + 2);
+
+//  variable dynamic
+  var x;
+  x = 8;
+  print(x);
+}
