05f8523e6be3c6178bddafcd4233f69828064273
itselaras <gunadermawan62@gmail.com>
2022-08-11 09:33:25 +0700

    course variable dynamix

--- bin/dart.dart
+++ bin/dart.dart
@@ -4,13 +4,20 @@ import 'dart:io';
 void main(List<String> arguments) {
   // hello world
   print('Hello world: ${dart.calculate()}!');
+
   // dart I/O
   print("input password");
   String? inputText = stdin.readLineSync()!;
   print("password: $inputText");
+
   // variable
   var name = "gunadermawan";
   print(name);
   var age = 12;
   print(age + 2);
+
+//  variable dynamic
+  var x;
+  x = 8;
+  print(x);
 }
