b792ee7bef4608ba8298963e84da1eebedb4920c
itselaras <gunadermawan62@gmail.com>
2022-08-12 09:43:43 +0700

    create case study temperature converter

---
+++ bin/fundamentals/convertTemperature.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+
+void main(){
+  stdout.write("masukan suhu dalam Fahrenhit: ");
+  var fahrenhit = num.parse(stdin.readLineSync()!);
+  var celcius = (fahrenhit -32) *5 /9;
+  print("$fahrenhit derajat Fahrenhit  = $celcius derajat celcius");
+}
