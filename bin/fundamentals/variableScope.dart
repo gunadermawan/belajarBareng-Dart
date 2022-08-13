b8f570bc9a84dd6fe54afbea500d03edccf97470
itselaras <gunadermawan62@gmail.com>
2022-08-13 09:03:16 +0700

    update course variable scope global and function

--- bin/fundamentals/variableScope.dart
+++ bin/fundamentals/variableScope.dart
@@ -1,10 +1,22 @@
-void main(){
+// variable global
+var price = 0;
+
+void main() {
   // all variables can be access cause in scope
   var isDiscount = true;
   var price = 30000;
-  var dicount = 0;
+  var dicount = checkDiscount(price);
   if (isDiscount) {
     dicount = (10 / 100 * price).toInt();
   }
   print("you need to pay: ${price - dicount}");
+}
+
+// variable function scope
+num checkDiscount(num price) {
+  num discount = 0;
+  if (price >= 100000) {
+    discount = ((10 / 100) * price);
+  }
+  return discount;
 }
