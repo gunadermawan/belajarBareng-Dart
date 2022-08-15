834e313e7579f5df34d61b8a743e64c6258a52d4
itselaras <gunadermawan62@gmail.com>
2022-08-15 10:33:04 +0700

    update course else if

--- bin/controlFlow/ifElse.dart
+++ bin/controlFlow/ifElse.dart
@@ -1,11 +1,29 @@
+import 'dart:io';
+
 void main(){
-  var isRaining = true;
-  
-  print("prepare for going to office");
-  if(isRaining) {
-    print("oh. it's raining, bring an umbrella");
+  // var isRaining = true;
+  //
+  // print("prepare for going to office");
+  // if(isRaining) {
+  //   print("oh. it's raining, bring an umbrella");
+  // } else {
+  //   print("its looks good with cloudy");
+  // }
+  // print("going to office");
+  stdout.write('input your value of assignment: ');
+  var score = num.parse(stdin.readLineSync()!);
+  print('your value now convert is: ${calculateScore(score)}');
+}
+
+
+String calculateScore(num score){
+  if(score > 90){
+    return 'A';
+  } else if(score > 80){
+    return 'B';
+  } else if(score > 70){
+    return 'C';
   } else {
-    print("its looks good with cloudy");
+    return 'D';
   }
-  print("going to office");
 }
