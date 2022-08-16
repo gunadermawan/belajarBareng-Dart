43e867edff7baa3bd6ae350a838ec019bc5d62a7
itselaras <gunadermawan62@gmail.com>
2022-08-16 13:34:13 +0700

    update course dowhile

--- bin/controlFlow/whileDoWhile.dart
+++ bin/controlFlow/whileDoWhile.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
   var i = 1;
   // while iteration
@@ -11,4 +13,15 @@ void main() {
     print(i);
     i++;
   } while (i <= 100);
+
+  String username;
+  bool notValid = false;
+  do {
+    stdout.write("Masukan nama Anda: ");
+    username = stdin.readLineSync() ?? "";
+    if (username.length < 6) {
+      notValid = true;
+      print("username Anda tidak valid");
+    }
+  } while (notValid);
 }
