9d5ecfd38536f2368fa19a4908973fa97b49b11e
itselaras <gunadermawan62@gmail.com>
2022-08-16 13:49:57 +0700

    update course: continue for looping

--- bin/controlFlow/breakAndContinue.dart
+++ bin/controlFlow/breakAndContinue.dart
@@ -14,3 +14,12 @@ void main() {
     print('$searchNumber bukan bilangan prima!');
   }
 }
+
+void breakContinue() {
+  for (int i = 0; i < 10; i++) {
+    if (i % 3 == 0) {
+      continue;
+    }
+    print(i);
+  }
+}
