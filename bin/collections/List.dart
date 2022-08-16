b4342561bce5570ee60813e5d89a6cb3dc2976c5
itselaras <gunadermawan62@gmail.com>
2022-08-16 15:56:08 +0700

    create course: kotlin collections

---
+++ bin/collections/List.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<int> numberListExample = [1, 2, 3, 4, 5];
+  var numberlist = [1, 2, 3, 4, 5];
+  var stringList = ['Hello', 'dicoding'];
+  for(int i = 0; i<numberlist.length;i++){
+    print(numberlist[i]);
+  }
+}
