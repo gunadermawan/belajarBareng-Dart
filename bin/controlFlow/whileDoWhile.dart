7ec25955dc8373861da03fd0d3e69061d5490e19
itselaras <gunadermawan62@gmail.com>
2022-08-16 13:19:02 +0700

    update course: whileDoWhile

---
+++ bin/controlFlow/whileDoWhile.dart
@@ -0,0 +1,14 @@
+void main() {
+  var i = 1;
+  // while iteration
+  while (i <= 100) {
+    print(i);
+    i++;
+  }
+
+//  do while iteration
+  do {
+    print(i);
+    i++;
+  } while (i <= 100);
+}
