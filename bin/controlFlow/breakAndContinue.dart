e0d7ba47792d0e4e0dbbb25665756447c348ccc9
itselaras <gunadermawan62@gmail.com>
2022-08-16 13:43:14 +0700

    update course: breakContinue

---
+++ bin/controlFlow/breakAndContinue.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+//  20 bil prima
+  var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43];
+  stdout.write("masukan angka prima: ");
+  var searchNumber = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < primeNumbers.length; i++) {
+    if (searchNumber == primeNumbers[i]) {
+      print("$searchNumber adalah bilangan prima ke-${i + 1}");
+      break;
+    }
+    print('$searchNumber bukan bilangan prima!');
+  }
+}
